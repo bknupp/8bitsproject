fffb2c9a328357f73bcc07c10b3edc693ac0a977
Pecota Bill Gates <eduardo.90.martins@gmail.com>
2013-02-12 12:54:18 -0200

    imagens

---
+++ js/megaman.js
@@ -0,0 +1,28 @@
+function mudaCorCutman() {
+	$('.capacete').css('background','gray');
+	$('.tronco').css('background', 'silver');
+}
+
+function mudaCorElecman() {
+	$('.capacete').css('background','#008B8B');
+	$('.tronco').css('background','#F0DC86');
+}
+
+function mudaCorGutsman() {
+	$('.capacete').css('background','#DAA520');
+	$('.tronco').css('background','#FFFAFA');
+}
+
+function mudaCorFireman() {
+	$('.capacete').css('background','#DAA520');
+	$('.tronco').css('background','#FFFAFA');
+}
+
+function mudaCorIceman() {
+	$('.capacete').css('background','#DAA520');
+	$('.tronco').css('background','#FFFAFA');
+}
+function mudaCorBombman() {
+	$('.capacete').css('background','#DAA520');
+	$('.tronco').css('background','#FFFAFA');
+}
